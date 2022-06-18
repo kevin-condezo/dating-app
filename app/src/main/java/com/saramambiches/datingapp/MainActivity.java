e8513170fdcbbfa721afef0691e8a40d029fe435
Martin <edwin.anton@unmsm.edu.pe>
2022-06-18 11:55:39 -0500

    Ajustes no tan notorios

--- app/src/main/java/com/saramambiches/datingapp/MainActivity.java
+++ app/src/main/java/com/saramambiches/datingapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.saramambiches.datingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //Ejecutando RegisterAndLogin
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -20,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        },2000);
+        },1500);
     }
+
 }
--- app/src/main/java/com/saramambiches/datingapp/RegisterAndLogin.java
+++ app/src/main/java/com/saramambiches/datingapp/RegisterAndLogin.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.Slide;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +18,8 @@ public class RegisterAndLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_and_login);
+
+
         btredirect=(TextView)findViewById(R.id.bt_redirect);
         btlogin= (Button) findViewById(R.id.bt_login);
 
