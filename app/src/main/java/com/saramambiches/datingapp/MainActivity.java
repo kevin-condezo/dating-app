46fe1fb422da27ff538d48894ebb91d86ae59e7c
Martin <edwin.anton@unmsm.edu.pe>
2022-06-19 17:44:52 -0500

    Inicio Mejorado

--- app/src/main/java/com/saramambiches/datingapp/MainActivity.java
+++ app/src/main/java/com/saramambiches/datingapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.transition.Slide;
 import android.transition.Transition;
 import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //Ejecutando RegisterAndLogin
         new Handler().postDelayed(new Runnable() {
             @Override
--- app/src/main/java/com/saramambiches/datingapp/RegisterAndLogin.java
+++ app/src/main/java/com/saramambiches/datingapp/RegisterAndLogin.java
@@ -11,12 +11,31 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegisterAndLogin extends AppCompatActivity {
     Button btlogin, btregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_and_login);
+
+        ImageSlider Imagenes=findViewById(R.id.slider);
+
+        List<SlideModel> slideModels=new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.im1));
+        slideModels.add(new SlideModel(R.drawable.im2));
+        slideModels.add(new SlideModel(R.drawable.im3));
+        slideModels.add(new SlideModel(R.drawable.im4));
+        slideModels.add(new SlideModel(R.drawable.im5));
+        slideModels.add(new SlideModel(R.drawable.im6));
+
+        Imagenes.setImageList(slideModels, true);
 
 
         btlogin= (Button) findViewById(R.id.bt_login);
