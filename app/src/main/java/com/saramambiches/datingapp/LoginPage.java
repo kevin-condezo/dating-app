8806a0281c8f4c1407cf5cdcf53dd4a10761e938
Martin <edwin.anton@unmsm.edu.pe>
2022-06-22 11:04:44 -0500

    Barra de estado cambiada

--- app/src/main/java/com/saramambiches/datingapp/LoginPage.java
+++ app/src/main/java/com/saramambiches/datingapp/LoginPage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.telephony.TelephonyCallback;
 import android.view.View;
