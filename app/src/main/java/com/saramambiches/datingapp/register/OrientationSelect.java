8ab66ad9a1c3c508abdcd43b1540c456ad456ed5
Martin <edwin.anton@unmsm.edu.pe>
2022-07-12 09:42:43 -0500

    add sex and orientation layout

--- app/src/main/java/com/saramambiches/datingapp/register/OrientationSelect.java
+++ app/src/main/java/com/saramambiches/datingapp/register/OrientationSelect.java
@@ -1,18 +1,18 @@
 package com.saramambiches.datingapp.register;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.RadioGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.saramambiches.datingapp.R;
 
-public class GenderSelect extends AppCompatActivity {
+public class OrientationSelect extends AppCompatActivity {
     private RadioGroup r_RadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gender_select);
+        setContentView(R.layout.activity_orientation_select);
         r_RadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
 
     }
