4784bc5b6e2e73c2df95207a1eb0c684b4fa7e45
Martin <edwin.anton@unmsm.edu.pe>
2022-07-11 22:54:17 -0500

    New layout

---
+++ app/src/main/java/com/saramambiches/datingapp/register/GenderSelect.java
@@ -0,0 +1,16 @@
+package com.saramambiches.datingapp.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saramambiches.datingapp.R;
+
+public class GenderSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gender_select);
+    }
+}
