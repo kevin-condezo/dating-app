a2a790947764d0f78ac916d12dada6a0e118c909
JeffCode10 <jeff06maga@gmail.com>
2022-08-07 13:24:00 -0500

    Actualización Matches Finished

--- app/src/main/java/com/saramambiches/datingapp/Messages.java
+++ app/src/main/java/com/saramambiches/datingapp/Messages.java
@@ -11,6 +11,11 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.saramambiches.datingapp.Matches.MatchesAdapter;
 import com.saramambiches.datingapp.Matches.MatchesObject;
 
@@ -22,6 +27,7 @@ public class Messages extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mMatchesAdapter;
     private RecyclerView.LayoutManager mMatchesLayoutManager;
+    private String currentUserId; //= mAuth.getCurrentUser().getUid(); // get current user id
     BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,7 @@ public class Messages extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
 
-
+        currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
         mRecyclerView.setHasFixedSize(true);
@@ -37,19 +43,15 @@ public class Messages extends AppCompatActivity {
         mRecyclerView.setLayoutManager(mMatchesLayoutManager);
         mMatchesAdapter = new MatchesAdapter(getDataSetMatches(), Messages.this);
         mRecyclerView.setAdapter(mMatchesAdapter);
+        getUserMatchId();
+
         /*
         MatchesObject obj = new MatchesObject("asd");
         resultsMatches.add(obj);
-        resultsMatches.add(obj);
-        resultsMatches.add(obj);
-        resultsMatches.add(obj);
         */
 
-        for(int i=0; i<100; i++){
-            MatchesObject obj = new MatchesObject(Integer.toString(i));
-            resultsMatches.add(obj);
-        }
-        mMatchesAdapter.notifyDataSetChanged();
+
+       // mMatchesAdapter.notifyDataSetChanged();
 
         //Navigation Bar
 
@@ -82,6 +84,55 @@ public class Messages extends AppCompatActivity {
 
 
         //-----------
+    }
+
+    private void getUserMatchId() {
+
+        DatabaseReference matchDb = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId).child("connections").child("matches");
+        matchDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot match : snapshot.getChildren()){
+                        FetchMatchInformation(match.getKey());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void FetchMatchInformation(String key) {
+        DatabaseReference userDb = FirebaseDatabase.getInstance().getReference().child("Users").child(key);
+        userDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String userId = snapshot.getKey();
+                    String name = "";
+                    String profileImageUrl = "";
+
+                    if(snapshot.child("name").getValue()!=null){
+                        name = snapshot.child("name").getValue().toString();
+                    }
+                    if(snapshot.child("profileImageUrl").getValue()!=null){
+                        profileImageUrl = snapshot.child("profileImageUrl").getValue().toString();
+                    }
+                    MatchesObject obj = new MatchesObject(userId, name, profileImageUrl);
+                    resultsMatches.add(obj);
+                    mMatchesAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private ArrayList<MatchesObject> resultsMatches = new ArrayList<MatchesObject>();
